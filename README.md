9b40f6d091a256ccb06c85a97d904a906eae3f67
yoezequiel <ezequielcasiano15@gmail.com>
2024-05-17 15:41:18 -0300

    new colors, add readme

---
+++ README.md
@@ -0,0 +1,47 @@
+# Version lite de neofetch y fastfetch
+
+Es una herramienta escrita en Python que recopila y muestra información detallada sobre el sistema operativo y el hardware de un sistema Linux. Proporciona una visión general rápida y útil de diversos aspectos del sistema, como la distribución de Linux, el kernel, el uso de la CPU, la memoria, el almacenamiento, entre otros.
+
+## Funcionalidades
+
+-   Muestra información detallada sobre el sistema operativo, incluida la distribución de Linux y la versión del kernel.
+-   Proporciona datos sobre el uso de la CPU, incluido el nombre del modelo.
+-   Informa sobre el uso de la memoria RAM y el swap.
+-   Muestra el estado y nivel de carga de la batería (si está disponible).
+-   Proporciona información sobre el espacio en disco y las particiones montadas.
+-   Muestra la dirección IP local y el nombre del usuario del sistema.
+
+## Requisitos
+
+-   Python 3.x
+-   psutil
+
+## Uso
+
+### Paquete
+
+Puedes descargar el .deb a través del siguiente enlace: [yofetch](https://github.com/yoezequiel/yofetch/releases).
+
+### Codigo
+
+1. Clona este repositorio en tu máquina local:
+
+    ```bash
+    git clone https://github.com/yoezequiel/yofetch.git
+    ```
+
+2. Instala las dependencias necesarias:
+
+    ```bash
+    pip install -r requirements.txt
+    ```
+
+3. Ejecuta la aplicación Flask:
+
+    ```bash
+    python app.py
+    ```
+
+## Contribuciones
+
+Las contribuciones son bienvenidas. Si encuentras un error o deseas mejorar la funcionalidad, no dudes en abrir un issue o enviar un pull request.
--- app.py
+++ app.py
@@ -13,12 +13,12 @@ def get_cpu_name():
 
 def get_memory_usage():
     vm = psutil.virtual_memory()
-    return f"\033[36m{vm.used / (1024**3):.2f} GB / {vm.total / (1024**3):.2f} GB\033[0m (\033[35m{vm.percent}%\033[0m)"
+    return f"{vm.used / (1024**3):.2f} GB / {vm.total / (1024**3):.2f} GB\033[0m (\033[96m{vm.percent}%\033[0m)"
 
 
 def get_swap_usage():
     swap = psutil.swap_memory()
-    return f"\033[36m{swap.free / (1024**3):.2f} GB / {swap.total / (1024**3):.2f} GB\033[0m (\033[35m{swap.percent}%\033[0m)"
+    return f"{swap.free / (1024**3):.2f} GB / {swap.total / (1024**3):.2f} GB\033[0m (\033[96m{swap.percent}%\033[0m)"
 
 
 def get_disk_usage():
@@ -26,7 +26,7 @@ def get_disk_usage():
     disk_usage_str = ""
     for partition in partitions:
         usage = psutil.disk_usage(partition.mountpoint)
-        disk_usage_str += f"\033[93m{partition.mountpoint}\033[0m: \033[96m{usage.free / (1024**3):.2f} GB / {usage.total / (1024**3):.2f} GB\033[0m (\033[91m{usage.percent}%\033[0m)\n"
+        disk_usage_str += f"\033[92m{partition.mountpoint}\033[0m: \033[93m{usage.free / (1024**3):.2f} GB / {usage.total / (1024**3):.2f} GB\033[0m (\033[91m{usage.percent}%\033[0m)\n"
     return disk_usage_str.strip()
 
 
@@ -64,11 +64,25 @@ def get_local_ip():
         return f"Error: {str(e)}"
 
 
+def get_uptime():
+    uptime_output = subprocess.getoutput("uptime -p")
+    uptime = " ".join(word for word in uptime_output.split() if word != "up")
+    return uptime
+
+
+def get_username():
+    username = subprocess.getoutput("whoami")
+    pcname = subprocess.getoutput("hostname")
+    return username, pcname
+
+
+username, pcname = get_username()
+
 distro_id, distro_version = get_linux_distribution()
 system_info = {
-    "Operating System": f"\033[91m{distro_id.capitalize()}\033[0m ({distro_version})",
+    "Operating System": f"{distro_id.capitalize()}{distro_version}",
     "Kernel": platform.uname().release,
-    "Uptime": subprocess.getoutput("uptime -p"),
+    "Uptime": get_uptime(),
     "Packages": subprocess.getoutput("dpkg-query -f '.\n' -W | wc -l"),
     "Shell": os.path.basename(os.environ.get("SHELL")),
     "Display": subprocess.getoutput("xrandr | grep '*' | awk '{print $1}'"),
@@ -80,6 +94,7 @@ system_info = {
     "Swap": get_swap_usage(),
     "Disks": get_disk_usage(),
 }
-
+print(f"\033[94m{username}\033[0m@\033[93m{pcname}\033[0m")
+print(f"\033[94m-" * 35)
 for key, value in system_info.items():
-    print(f"\033[94m{key}:\033[0m {value}")
+    print(f"\033[94m{key}:\033[93m {value}")
